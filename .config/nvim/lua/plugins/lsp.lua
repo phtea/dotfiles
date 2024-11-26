5c971c4e84c007202954a45c601a2556addd313b
phtea <cerejapushmenkov2@gmail.com>
2024-11-26 19:42:04 +0300

    fixed bashrc

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -83,6 +83,8 @@ function M.setup(use)
         gopls = "gopls",
     }
 
+    local missing_servers = {}
+
     for server, cmd in pairs(servers) do
         if executable(cmd) then
             lspconfig[server].setup({
@@ -90,12 +92,15 @@ function M.setup(use)
                 on_attach = on_attach,
             })
         else
-            vim.schedule(function()
-                vim.notify("LSP server '" .. server .. "' is not installed. Skipping.", vim.log.levels.WARN)
-            end)
+            table.insert(missing_servers, server)
         end
     end
 
+    if #missing_servers > 0 then
+        vim.schedule(function()
+            vim.notify("Missing LSP servers: " .. table.concat(missing_servers, ", "), vim.log.levels.WARN)
+        end)
+    end
     -- Additional diagnostic settings (optional)
     vim.diagnostic.config({
         virtual_text = true,
