66b1f68aa0813706e67a72a76db33228cb145087
phtea <cerejapushmenkov2@gmail.com>
2024-11-29 13:40:33 +0300

    migrated to lazy with minimal config

--- .config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return function(use)
-    use {
-        'Mofiqul/vscode.nvim',
-        config = function()
-            vim.cmd.colorscheme("vscode")
-        end
-    }
-end
--- .config/nvim/lua/plugins/faster.lua
+++
@@ -1,3 +0,0 @@
-return function(use)
-    use 'pteroctopus/faster.nvim'
-end
