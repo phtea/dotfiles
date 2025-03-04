75f576f807f816ed5b3d99f6b5d6f98483682c28
phtea <cerejapushmenkov2@gmail.com>
2025-03-05 01:51:22 +0300

    i think im good now

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,16 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPre", "BufNewFile" },
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                highlight = { enable = true, },
-                indent = { enable = true },
-                ensure_installed = {
-                    "bash", "c", "diff", "html", "javascript",
-                    "json", "markdown", "markdown_inline", "lua", "python", "go",
-					"vim", "vimdoc", "regex",
-                },
-            })
-        end,
-    },
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = { enable = true, },
+			indent = { enable = true },
+			ensure_installed = {
+				"bash", "c", "diff", "html", "javascript",
+				"json", "markdown", "markdown_inline", "lua", "python", "go",
+				"vim", "vimdoc", "regex",
+			},
+		})
+	end,
 }
