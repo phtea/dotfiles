dc58e1a1a4231be9dd808f86998883bed4767b6e
phtea <cerejapushmenkov2@gmail.com>
2025-03-08 02:48:29 +0300

    cmake ext, changed theme to tokyodark, changed fugitive a bit

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,29 +1,11 @@
 return {
-	'navarasu/onedark.nvim',
-	name = 'onedark',
-	lazy = false,
-	priority = 1000,
-	config = function(_, opts)
-		require('onedark').setup(opts)
-		require('onedark').load()
+	"tiagovla/tokyodark.nvim",
+	config = function()
+		vim.cmd [[
+			colorscheme tokyodark
+			highlight StatusLine guifg=Normal guibg=NONE gui=bold
+			highlight StatusLineNC guifg=Normal guibg=NONE
+			highlight ModeMsg guifg=NONE guibg=NONE
+		]]
 	end,
-	opts = {
-		colors = {
-			black = '#131019',
-			bg0 = '#1E1E28',
-			bg1 = '#2d2939',
-			bg2 = '#332E41',
-			bg3 = '#393348',
-			bg_d = '#1B1923',
-		},
-		highlights = {
-			['@comment'] = { fg = '$blue' },
-			['@operator'] = { fg = '$cyan' },
-			['@lsp.type.comment'] = { fg = '$blue' },
-			['@lsp.type.variable'] = { fg = '$yellow' },
-			['@lsp.type.property'] = { fg = '$red' },
-			['@lsp.type.generic'] = { fg = '$red' },
-			['CursorLineNr'] = { fg = '$blue', bold = true },
-		},
-	},
 }
