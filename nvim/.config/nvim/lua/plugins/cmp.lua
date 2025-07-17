f68e7b2c0953ccb972168d8ff745fdae09e6afee
tea <tea>
2025-07-17 21:48:56 +0300

    Migrated to stow

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,19 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = '*',
+    opts = {
+	keymap = { preset = "super-tab", },
+	signature = {
+	    enabled = true,
+	    window = { show_documentation = true, },
+	},
+
+	cmdline = { enabled = false, }, -- disable in command mode!!
+
+	completion = {
+	    documentation = { auto_show = true, auto_show_delay_ms = 100, },
+	    trigger = { show_in_snippet = false, },
+	}
+    },
+}
