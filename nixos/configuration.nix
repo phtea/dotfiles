38a8c7ea1e17af59cc2d6e880203b3225cbd09ad
Pushmenkov Sergey <cerejapushmenkov2@gmail.com>
2025-07-19 19:28:58 +0300

    testing new things now

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,88 +1,105 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
+    imports = [ ./hardware-configuration.nix ];
 
-  system.stateVersion = "24.05";
-  nixpkgs.config.allowUnfree = true;
+    system.stateVersion = "24.05";
+    nixpkgs.config.allowUnfree = true;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.efiSysMountPoint = "/boot";
 
-  networking.hostName = "nixvm";
-  networking.networkmanager.enable = true;
+    networking.hostName = "nix";
+    networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Moscow";
-  i18n.defaultLocale = "en_US.UTF-8";
-  console.keyMap = "us";
+    time.timeZone = "Europe/Moscow";
+    i18n.defaultLocale = "en_US.UTF-8";
+    console.keyMap = "us";
 
-  users.users.phtea = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    shell = pkgs.bash;
-    packages = with pkgs; [
-      firefox
-      neovim
-      git
-      unzip
-      wget
-      rofi
+    users.users.phtea = {
+	isNormalUser = true;
+	extraGroups = [ "wheel" "networkmanager" ];
+	shell = pkgs.bash;
+	packages = with pkgs; [
+	    firefox
+	    neovim
+	    git
+	    unzip
+	    wget
+	    rofi
+	];
+    };
+
+    security.sudo.enable = true;
+
+    services.xserver.enable = true;
+    services.xserver.windowManager.i3.enable = true;
+    services.xserver.displayManager.startx.enable = true;
+    services.xserver.xkb.layout = "us";
+
+    services.pipewire = {
+	enable = true;
+	audio.enable = true;
+	alsa.enable = true;
+	pulse.enable = true;
+    };
+
+    fonts.packages = with pkgs; [
+	fontconfig
+	nerd-fonts.fira-code
+	nerd-fonts.jetbrains-mono
+	noto-fonts-emoji
     ];
-  };
 
-  security.sudo.enable = true;
+    environment.systemPackages = with pkgs; [
+	xclip
+	ghostty
+	neovim
+	firefox
+	git
+	htop
+	zsh
+	tmux
+	xterm
+	rofi
+	i3status
+	dmenu
+	stow
+	gcc
+	gnumake
+	vim_configurable
+	ripgrep
+	fastfetch
+	scrot
+	telegram-desktop
+	discord
+	picom
+	tree
+	feh
+	xss-lock
+	i3lock
+	xorg.xinit
+	xorg.xset
+	xfce.thunar
+	mpv
+	polybar
+	playerctl
+	pamixer
+	brightnessctl
+	fzf
+	fzf-obc
+	i3ipc-glib
+	jsoncpp
+    ];
 
-  services.xserver.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.displayManager.startx.enable = true;
-  services.xserver.xkb.layout = "us";
+	#    nixpkgs.overlays = [
+	# (final: prev: {
+	#     polybar = prev.polybar.override {
+	# 	i3Support = true;
+	#     };
+	# })
+	#    ];
 
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-  };
-
-  fonts.packages = with pkgs; [
-	  fontconfig
-	  nerd-fonts.fira-code
-	  nerd-fonts.jetbrains-mono
-  ];
-
-  environment.systemPackages = with pkgs; [
-    xclip
-    ghostty
-    neovim
-    firefox
-    git
-    htop
-    zsh
-    tmux
-    xterm
-    rofi
-    i3status
-    dmenu
-    stow
-    gcc
-    gnumake
-    vim_configurable
-    ripgrep
-    fastfetch
-    scrot
-    telegram-desktop
-    discord
-    picom
-    tree
-    feh
-    xss-lock
-    i3lock
-    xorg.xinit
-    xorg.xset
-    xfce.thunar
-    mpv
-  ];
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
--- nvim/.config/nvim/lua/theme.lua
+++ nvim/.config/nvim/lua/theme.lua
@@ -1,20 +1,20 @@
 -- Colorscheme
-vim.cmd.colorscheme("wildcharm")
+vim.cmd.colorscheme("slate")
 
--- Transparent background
-vim.api.nvim_set_hl(0, "Normal",      { bg = "NONE", ctermbg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalNC",    { bg = "NONE", ctermbg = "NONE" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
-
-
--- Make NormalFloat transparent
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-
--- Line numbers
-vim.api.nvim_set_hl(0, "LineNr",      { fg = "#80a0ff", ctermfg = "LightBlue" })
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "Gray", ctermfg = "Gray" })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "Gray", ctermfg = "Gray" })
+-- -- Transparent background
+-- vim.api.nvim_set_hl(0, "Normal",      { bg = "NONE", ctermbg = "NONE" })
+-- vim.api.nvim_set_hl(0, "NormalNC",    { bg = "NONE", ctermbg = "NONE" })
+-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
+--
+--
+-- -- Make NormalFloat transparent
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+--
+-- -- Line numbers
+-- vim.api.nvim_set_hl(0, "LineNr",      { fg = "#80a0ff", ctermfg = "LightBlue" })
+-- vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "Gray", ctermfg = "Gray" })
+-- vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "Gray", ctermfg = "Gray" })
 
 -- Sign column always visible
 vim.opt.signcolumn = "yes"
