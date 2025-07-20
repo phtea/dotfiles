61d753e9c9ec4919dbd8183947342845bf2ffa48
Pushmenkov Sergey <cerejapushmenkov2@gmail.com>
2025-07-20 14:21:27 +0300

    working arch setup

--- arch_bootstrapper.sh
+++ arch_bootstrapper.sh
@@ -7,22 +7,22 @@ sudo pacman -Syu --needed
 echo "=== Installing core packages ==="
 sudo pacman -S --needed \
   base-devel git \
-  neovim ripgrep fd \
-  fzf entr \
-  tmux \
-  ghostty \
-  polybar pamixer acpi \
-  picom \
-  rofi \
+  gvim neovim ripgrep fd \
+  stow fzf entr tmux ghostty polybar pamixer acpi picom rofi \
   ttf-font-awesome ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common noto-fonts-emoji \
-  networkmanager bluez blueman \
+  pipewire pipewire-pulse wireplumber networkmanager bluez blueman \
   xorg-server xorg-xinit xterm xorg-xrandr xorg-xset xorg-xprop xorg-xinput xorg-xkbcomp xorg-xauth xorg-xmodmap xorg-xrdb \
   i3-wm i3lock xss-lock dunst dex feh scrot xclip brightnessctl \
-  pipewire pipewire-pulse \
-  libnotify
+  libnotify thunar  \
 
 echo "=== Enabling necessary services ==="
-sudo systemctl enable --now NetworkManager bluetooth pipewire pipewire-pulse
+sudo systemctl enable --now NetworkManager bluetooth
+
+echo "=== Enabling user-level services ==="
+
+sudo loginctl enable-linger "$USER"
+
+systemctl --user enable --now pipewire.socket pipewire-pulse.socket
 
 echo "=== Stowing dotfiles ==="
 cd ~/dotfiles
