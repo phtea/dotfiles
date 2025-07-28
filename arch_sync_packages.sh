ec2fd88a8998a7d603f2c48b839f27fc8f274813
phtea <phtea@mail.com>
2025-07-29 00:32:23 +0300

    added arch_linux.png image as default wallpaper

--- arch_sync_packages.sh
+++ arch_sync_packages.sh
@@ -6,7 +6,7 @@ sudo pacman -S --needed \
   base-devel git \
   gvim neovim ripgrep fd \
   stow fzf entr tmux ghostty polybar pamixer acpi picom rofi rofi-emoji \
-  ttf-font-awesome ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common noto-fonts-emoji \
+  ttf-font-awesome ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common noto-fonts-emoji ttf-fira-code \
   pipewire pipewire-pulse wireplumber networkmanager bluez blueman \
   xorg-server xorg-xinit xterm xorg-xrandr xorg-xset xorg-xprop xorg-xinput xorg-xkbcomp xorg-xauth xorg-xmodmap xorg-xrdb \
   i3-wm i3lock xss-lock dunst dex feh scrot xclip brightnessctl \
