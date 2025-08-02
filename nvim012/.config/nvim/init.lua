14fa0355a32d39f0baa7fa90e29a20d7bcc01e83
phtea <phtea@mail.com>
2025-08-03 00:33:38 +0300

    deprecated nvim012, played around with it, but now it's just getting confusing at this point

--- nvim012/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- nvim config for nvim 0.12+ (hyped for release 😃)
-package.path = vim.fn.stdpath("config") .. "/?.lua;" .. package.path -- Enable local Lua modules
-
-require("core.mappings")
-require("core.options")
-require("core.lsp")
-require("core.pack-plugins")
--- nvim012/.config/nvim/lsp/solargraph.lua
+++
@@ -1,6 +0,0 @@
-return {
-    cmd = { 'solargraph_global', 'stdio', },
-    filetypes = { 'ruby', 'rake', 'gemfile', 'ru', 'rb', 'builder', 'gemspec', 'podspec', 'thor', },
-    root_markers = { '.git', 'Gemfile', '.solargraph.yml' },
-    settings = { solargraph = { diagnostics = true } },
-}
