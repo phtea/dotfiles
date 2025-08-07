479e43208dff7aa6777a6d143d64b0a9ab8bdf64
spushmenkov <spushmenkov@phoenixit.ru>
2025-08-07 12:15:25 +0300

    off toggle_current_line_blame by default

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
 	require('gitsigns').setup {
-	  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	  current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
