9d604f4445a63f5f93876277ced882abd89d0746
phtea <phtea@mail.com>
2025-08-10 21:13:31 +0300

    vscode theme is back (its just so good OTB)

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,6 @@
 return {
-  "vague2k/vague.nvim",
-  config = function()
-	local function persistent_hl()
-	  -- Vim illuminate
-	  vim.api.nvim_set_hl(0, "IlluminatedWordText",  { bg = "#1F1F1F", underline = false })
-	  vim.api.nvim_set_hl(0, "IlluminatedWordRead",  { bg = "#1F1F1F", underline = false })
-	  vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#1F1F1F", underline = false })
-	  -- Statusline
-	  vim.api.nvim_set_hl(0, "StatusLine", { bg = "" })
-	end
-
-	require("vague").setup({
-	  transparent = true,
-	  italic = false,
-	})
-
-	vim.cmd("colorscheme vague")
-	persistent_hl()
-  end
+  "Mofiqul/vscode.nvim",
+  config = function ()
+	vim.cmd [[colo vscode]]
+  end,
 }
