0002b4f061aa464972c35e9f6426fe0776ffd4ec
phtea <phtea@mail.com>
2025-08-12 01:18:53 +0300

    wow.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
   "Mofiqul/vscode.nvim",
-  config = function ()
-	vim.cmd [[colorscheme vscode]]
-	vim.cmd [[set cursorline]]
+  config = function()
+    vim.cmd [[colorscheme vscode]]
+    vim.cmd [[set cursorline]]
   end,
 }
