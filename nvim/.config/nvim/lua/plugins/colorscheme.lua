3049199f21cfc574688c11eb2c247c0f9b5d3bb3
phtea <phtea@mail.com>
2025-08-23 17:46:44 +0300

    kanso theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
   {
 	"Mofiqul/vscode.nvim",
+	enabled = false,
 	config = function()
 	  vim.cmd [[colorscheme vscode]]
 	  vim.cmd [[set cursorline]]
 	end,
   },
+  {
+	"webhooked/kanso.nvim",
+	config = function ()
+	  require('kanso').setup({
+		italics = false,
+	  })
+	  vim.cmd [[colorscheme kanso-zen]]
+	end
+  }
 }
