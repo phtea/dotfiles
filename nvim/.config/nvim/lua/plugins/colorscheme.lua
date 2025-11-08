e6723b685e99735f8044986561e875958cdb5a0c
phtea <phtea@mail.com>
2025-11-09 02:48:21 +0300

    using vague for now

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,42 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require('rose-pine').setup({
-			styles = {
+	{
+		"rose-pine/neovim",
+		enabled = false,
+		name = "rose-pine",
+		config = function()
+			require('rose-pine').setup({
+				styles = {
+					bold = false,
+					italic = false,
+				},
+				highlight_groups = {
+					NormalFloat = { link = "Normal" },
+					FloatBorder = { fg = "muted", bg = "none" },
+					FloatTitle = { bg = "base" },
+					StatusLine = { bg = "#1F1D2E" },
+					StatusLineNC = { bg = "#1F1D2E" },
+					DiagnosticVirtualTextInfo = { bg = "none" },
+					Pmenu = { bg = "none", },
+					DiffAdd = { fg = "#88D8AA" },
+					GitSignsAdd = { fg = "#88D8AA" },
+					GitSignsChange = { fg = "#F6C177" },
+					GitSignsDelete = { fg = "#F02961" },
+					WordSelected =  { bg = '#3c394e' },
+				},
+			})
+			vim.cmd.colorscheme('rose-pine')
+		end
+	},
+	{
+		"vague-theme/vague.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("vague").setup({
 				bold = false,
 				italic = false,
-			},
-			highlight_groups = {
-				NormalFloat = { link = "Normal" },
-				FloatBorder = { fg = "muted", bg = "none" },
-				FloatTitle = { bg = "base" },
-				StatusLine = { bg = "#1F1D2E" },
-				StatusLineNC = { bg = "#1F1D2E" },
-				DiagnosticVirtualTextInfo = { bg = "none" },
-				Pmenu = { bg = "none", },
-				DiffAdd = { fg = "#88D8AA" },
-				GitSignsAdd = { fg = "#88D8AA" },
-				GitSignsChange = { fg = "#F6C177" },
-				GitSignsDelete = { fg = "#F02961" },
-				WordSelected =  { bg = '#3c394e' },
-			},
-		})
-		vim.cmd.colorscheme('rose-pine')
-	end
+			})
+			vim.cmd("colorscheme vague")
+		end
+	},
 }
