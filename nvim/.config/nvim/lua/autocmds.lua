457aa3a878c4620c6fdb6d826c2710c0c131cfe4
phtea <phtea@mail.com>
2025-11-11 00:44:58 +0300

    notes ephemeral

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -39,3 +39,20 @@ vim.api.nvim_create_autocmd("BufRead", {
 		vim.bo.filetype = "json"
 	end,
 })
+
+local notes_path = vim.fn.expand("~/notes.md")
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  group = vim.api.nvim_create_augroup("ephemeral_notes", { clear = true }),
+  callback = function(args)
+    local name = vim.api.nvim_buf_get_name(args.buf)
+    if name == notes_path then
+      -- delete after the leave finishes to avoid weirdness
+      vim.schedule(function()
+        if vim.api.nvim_buf_is_valid(args.buf) then
+          vim.api.nvim_buf_delete(args.buf, { force = true })
+        end
+      end)
+    end
+  end,
+})
