23305d3af9f7f1b376f1bc9f747f85685598832e
spushmenkov <spushmenkov@phoenixit.ru>
2025-11-11 00:44:42 +0300

    yep

--- nvim/.config/nvim/ftplugin/qf.lua
+++ nvim/.config/nvim/ftplugin/qf.lua
@@ -2,4 +2,4 @@
 vim.keymap.set("n", "D", ":g//d<CR>", { buffer = true, silent = true })
 
 -- gd => :g!//d -> [K]eep all matching lines
-vim.keymap.set("n", "k", ":g!//d<cr>", { buffer = true, silent = true })
+vim.keymap.set("n", "K", ":g!//d<cr>", { buffer = true, silent = true })
