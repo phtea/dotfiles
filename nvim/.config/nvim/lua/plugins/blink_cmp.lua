fe3f05a3fdd9aaea4a5aaa7d073df7dfb733ba13
spushmenkov <spushmenkov@phoenixit.ru>
2025-11-24 13:32:28 +0300

    update plugins

--- nvim/.config/nvim/lua/plugins/blink_cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink_cmp.lua
@@ -1,4 +1,8 @@
-vim.pack.add({{src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.0')}})
+vim.pack.add({{
+	src = 'https://github.com/saghen/blink.cmp',
+	version = vim.version.range('^1')
+}})
+
 require("blink.cmp").setup({
 	keymap = { preset = 'super-tab' },
 	appearance = { nerd_font_variant = 'mono', },
