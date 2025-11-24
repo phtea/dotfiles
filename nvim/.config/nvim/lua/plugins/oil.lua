094a9345c4b8632c1038747f9758e44f37648708
spushmenkov <spushmenkov@phoenixit.ru>
2025-11-24 13:48:04 +0300

    a little bit of oil adjustments

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,14 @@
 vim.pack.add({"https://github.com/stevearc/oil.nvim"})
 require("oil").setup({
-	view_options = { show_hidden = true, },
+	view_options = {
+		show_hidden = true,
+	},
 	skip_confirm_for_simple_edits = true,
+	use_default_keymaps = false,
+	keymaps = {
+		["<CR>"] = "actions.select",
+		["-"] = { "actions.parent", mode = "n" },
+	}
 })
 
-vim.keymap.set('n', '-', "<cmd>Oil<cr>", {desc = "Oil: Open parent directory"})
+vim.keymap.set('n', '-', "<CMD>Oil<CR>", { desc = "Oil: Open parent directory" })
