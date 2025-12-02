a0e360e63f82d4bf1ba3b5707ffcc56f5b4cc331
phtea <phtea@mail.com>
2025-12-03 01:35:11 +0300

    compromiss

--- nvim/.config/nvim/lua/plugins/blink_cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink_cmp.lua
@@ -7,7 +7,17 @@ vim.pack.add({ {
 
 require("blink.cmp").setup({
 	keymap = {
-		preset = 'default'
+		preset = 'default',
+
+		['<Tab>'] = {
+			function(cmp)
+				if cmp.snippet_active() then return cmp.accept()
+				else return cmp.select_and_accept() end
+			end,
+			'snippet_forward',
+			'fallback'
+		},
+		['<S-Tab>'] = { 'snippet_backward', 'fallback' },
 	},
 	appearance = {
 		nerd_font_variant = 'mono',
