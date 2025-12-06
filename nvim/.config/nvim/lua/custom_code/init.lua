633e14259a8c8ad45f811b7270592eb88bc45c08
phtea <phtea@mail.com>
2025-12-06 13:43:59 +0300

    reload manually (faster and more explicit)

--- nvim/.config/nvim/lua/custom_code/init.lua
+++ nvim/.config/nvim/lua/custom_code/init.lua
@@ -1,17 +1,5 @@
--- Directory that contains this file and all custom_code config files
-local custom_code_dir = vim.fn.stdpath("config") .. "/lua/custom_code"
+require("custom_code.make")
+require("custom_code.reload_watcher")
 
--- Auto load custom_code settings
-for _, file in ipairs(vim.fn.readdir(custom_code_dir)) do
-	-- Only *.lua and skip this init.lua itself
-	if file:match("%.lua$") and file ~= "init.lua" then
-		local mod_name = file:gsub("%.lua$", "")
-		local ok, err = pcall(require, "custom_code." .. mod_name)
-		if not ok then
-			vim.notify(
-				"Error loading custom_code config: " .. mod_name .. "\n" .. err,
-				vim.log.levels.ERROR
-			)
-		end
-	end
-end
+-- deprecated
+-- require("custom_code.compile")
