157933d636020daad2d375f6b363043ac2d3f306
phtea <phtea@mail.com>
2025-12-06 13:46:38 +0300

    explicit plugin requiring

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,17 +1,14 @@
--- Directory that contains this file and all plugin config files
-local plugin_dir = vim.fn.stdpath("config") .. "/lua/plugins"
+-- has to go first, some plugins depend on it
+require("plugins.snacks")
 
--- Auto load plugins settings
-for _, file in ipairs(vim.fn.readdir(plugin_dir)) do
-	-- Only *.lua and skip this init.lua itself
-	if file:match("%.lua$") and file ~= "init.lua" then
-		local mod_name = file:gsub("%.lua$", "")
-		local ok, err = pcall(require, "plugins." .. mod_name)
-		if not ok then
-			vim.notify(
-				"Error loading plugin config: " .. mod_name .. "\n" .. err,
-				vim.log.levels.ERROR
-			)
-		end
-	end
-end
+require("plugins.autopairs")
+require("plugins.blink_cmp")
+require("plugins.colorscheme")
+require("plugins.gitsigns")
+require("plugins.grapple")
+require("plugins.local-highlight")
+require("plugins.lsp")
+require("plugins.oil")
+require("plugins.quicker")
+require("plugins.surround")
+require("plugins.treesitter")
