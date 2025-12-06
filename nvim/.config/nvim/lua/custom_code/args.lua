e01abd1a89fc1f79955a4a7cf894016aa68bc165
phtea <phtea@mail.com>
2025-12-06 17:40:41 +0300

    welcome args byebye grapple and harpoon :waving:

---
+++ nvim/.config/nvim/lua/custom_code/args.lua
@@ -0,0 +1,40 @@
+-- Replacement for Harpoon
+
+local show_args = function ()
+	vim.cmd [[
+		echo
+		args
+	]]
+end
+
+vim.keymap.set("n", "<leader>ma", function()
+	vim.cmd [[
+		argadd %
+		argdedup
+	]]
+	show_args()
+end, { desc = "Core: Add buffer to arglist" })
+
+vim.keymap.set("n", "<leader>mc", function()
+	vim.cmd [[
+		%argd
+	]]
+	show_args()
+end, { desc = "Core: Clear all current args" })
+
+vim.keymap.set("n", "<leader>mm", show_args, { desc = "Core: Show all current args" })
+
+vim.keymap.set("n", "<leader>md", function()
+	vim.cmd [[
+		silent! argdelete %
+	]]
+	show_args()
+end, { desc = "Core: Delete current arg" })
+
+-- <leader>1..9 -> jump to slot
+for i = 1, 9 do
+	vim.keymap.set("n", "<leader>" .. i, function()
+		vim.cmd("silent! " .. i .. "argument")
+		show_args()
+	end, { desc = ("Core: Select arg " .. i) })
+end
--- nvim/.config/nvim/lua/custom_code/init.lua
+++ nvim/.config/nvim/lua/custom_code/init.lua
@@ -1,2 +1,3 @@
 require("custom_code.execute")
 require("custom_code.reload_watcher")
+require("custom_code.args")
