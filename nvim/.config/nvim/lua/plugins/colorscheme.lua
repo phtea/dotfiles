98a35ba104f3f5674b826e8c327dd957c781f658
phtea <phtea@mail.com>
2025-12-07 23:08:42 +0300

    removed unused colorschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,11 @@
-vim.pack.add({
-	"https://github.com/vague-theme/vague.nvim",
-	"https://github.com/Mofiqul/vscode.nvim",
-})
-
-require("vague").setup({
-	bold = false,
-	italic = false,
-})
+vim.pack.add({ "https://github.com/Mofiqul/vscode.nvim", })
 
 local M = {}
 M.default = "vscode" -- default colorscheme
 
-local set_hl = vim.api.nvim_set_hl
-
 M.overrides = {}
 
-M.overrides.vague = function()
-	set_hl(0, "StatusLine", { bg = "none" })
-	set_hl(0, "StatusLineNC", { bg = "none" })
-	set_hl(0, "Search", { bg = "#405065", ctermbg = 11, ctermfg = 0, fg = "#CDCDCD" })
-	set_hl(0, "CurSearch", { bg = "#BFAFA9", fg = "#323232" })
-	set_hl(0, "IncSearch", { link = "CurSearch" })
-end
-
+local set_hl = vim.api.nvim_set_hl
 M.overrides.vscode = function()
 	set_hl(0, "StatusLine", { link = "StatusLineNC" })
 	set_hl(0, "Pmenu", { bg = "#252525" })
