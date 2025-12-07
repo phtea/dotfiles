dc99e7fcac3dccbfdc40ba4be6fd6cf02fa23c14
phtea <phtea@mail.com>
2025-12-07 21:14:45 +0300

    nice native autocomp
    autocomp

--- nvim/.config/nvim/lua/core/lsp.lua
+++ nvim/.config/nvim/lua/core/lsp.lua
@@ -1,5 +1,82 @@
 -- Native fuzzy popup filtering
 vim.o.completeopt = "menuone,noinsert,fuzzy,popup"
+vim.o.pumheight = 10 -- show only first 10 entries (less intrusive)
+
+-- LSP CompletionItemKind (1..25) -> icon
+local icon = {
+  [1]  = "󰉿", -- Text
+  [2]  = "󰆧", -- Method
+  [3]  = "󰊕", -- Function
+  [4]  = "",  -- Constructor
+  [5]  = "󰜢", -- Field
+  [6]  = "󰀫", -- Variable
+  [7]  = "󰠱", -- Class
+  [8]  = "",  -- Interface
+  [9]  = "󰏗", -- Module
+  [10] = "󰜢", -- Property
+  [11] = "󰑭", -- Unit
+  [12] = "󰎠", -- Value
+  [13] = "",  -- Enum
+  [14] = "󰌋", -- Keyword
+  [15] = "",  -- Snippet
+  [16] = "󰏘", -- Color
+  [17] = "󰈙", -- File
+  [18] = "󰈇", -- Reference
+  [19] = "󰉋", -- Folder
+  [20] = "",  -- EnumMember
+  [21] = "󰏿", -- Constant
+  [22] = "󰙅", -- Struct
+  [23] = "",  -- Event
+  [24] = "󰆕", -- Operator
+  [25] = "󰊄", -- TypeParameter
+}
+
+local kind_hl = {
+  [1]  = "PmenuKindText",
+  [2]  = "PmenuKindMethod",
+  [3]  = "PmenuKindFunction",
+  [4]  = "PmenuKindConstructor",
+  [5]  = "PmenuKindField",
+  [6]  = "PmenuKindVariable",
+  [7]  = "PmenuKindClass",
+  [8]  = "PmenuKindInterface",
+  [9]  = "PmenuKindModule",
+  [10] = "PmenuKindProperty",
+  [11] = "PmenuKindUnit",
+  [12] = "PmenuKindValue",
+  [13] = "PmenuKindEnum",
+  [14] = "PmenuKindKeyword",
+  [15] = "PmenuKindSnippet",
+  [16] = "PmenuKindColor",
+  [17] = "PmenuKindFile",
+  [18] = "PmenuKindReference",
+  [19] = "PmenuKindFolder",
+  [20] = "PmenuKindEnumMember",
+  [21] = "PmenuKindConstant",
+  [22] = "PmenuKindStruct",
+  [23] = "PmenuKindEvent",
+  [24] = "PmenuKindOperator",
+  [25] = "PmenuKindTypeParam",
+}
+
+-- Show signature help automatically
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client or not client:supports_method("textDocument/signatureHelp") then return end
+
+    vim.api.nvim_create_autocmd("InsertCharPre", {
+      buffer = ev.buf,
+      callback = function()
+				if vim.fn.pumvisible() == 1 then return end
+        local ch = vim.v.char
+        if ch == "(" or ch == "," then
+          vim.schedule(vim.lsp.buf.signature_help)
+        end
+      end,
+    })
+  end,
+})
 
 -- Enable native LSP completion + autotrigger when an LSP attaches
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -9,8 +86,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
 		client.server_capabilities.completionProvider.triggerCharacters = chars
-		vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
+
+		vim.lsp.completion.enable(true, client.id, _, {
 			autotrigger = true,
+			convert = function(item)
+				local k = item.kind
+				local ic = icon[k] or "·"
+				return {
+					-- Put icon abbr so it's on the left
+					abbr = ic,
+					abbr_hlgroup = kind_hl[k] or "",
+
+					-- Menu is label and detail now
+					menu = string.format("%s %s", item.label, item.detail),
+
+					-- Kind is empty since it's part of abbr
+					kind = "",
+				}
+			end,
 		})
 	end,
 })
