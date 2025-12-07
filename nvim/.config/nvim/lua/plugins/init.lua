9dae908b838ec6ad74cbf236cf9ff345734ea2f3
phtea <phtea@mail.com>
2025-12-07 14:58:22 +0300

    remove deprecated stuff

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,9 +2,7 @@
 require("plugins.snacks")
 
 require("plugins.autopairs")
--- require("plugins.blink_cmp")
 require("plugins.gitsigns")
--- require("plugins.grapple")
 require("plugins.local-highlight")
 require("plugins.lsp")
 require("plugins.oil")
