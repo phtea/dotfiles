b62380e9cb308ea7c86f1416268e2b1022acd0f6
phtea <phtea@mail.com>
2025-12-10 00:59:57 +0300

    deleted deprecation and added <leader>x for ":Dispatch "

--- nvim/.config/nvim/lua/custom_code/init.lua
+++ nvim/.config/nvim/lua/custom_code/init.lua
@@ -1,2 +1,1 @@
-require("custom_code.execute")
 require("custom_code.reload_watcher")
--- nvim/.config/nvim/lua/plugins/vim-dispatch.lua
+++ nvim/.config/nvim/lua/plugins/vim-dispatch.lua
@@ -2,3 +2,6 @@
 vim.pack.add({
 	"https://github.com/tpope/vim-dispatch"
 })
+
+-- E[x]ecute (d is taken by diagnostics)
+vim.keymap.set("n", "<leader>x", ":Dispatch ", { desc = "Command for compiling and quickfix-ing" })
