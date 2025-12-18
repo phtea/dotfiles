ba96989c1d31be1ed698ec82bde9784b33fff65e
spushmenkov <spushmenkov@phoenixit.ru>
2025-12-18 16:11:44 +0300

    more natural-feeling keybind

--- nvim/.config/nvim/lua/plugins/vim-highligher.lua
+++ nvim/.config/nvim/lua/plugins/vim-highligher.lua
@@ -1,6 +1,6 @@
 vim.pack.add({"https://github.com/azabiong/vim-highlighter"})
 
 vim.keymap.set("n", "<M-n>", function() vim.cmd("Hi>") end, { desc = "Vim-highlighter: Next", })
-vim.keymap.set("n", "<M-p>", function() vim.cmd("Hi<") end, { desc = "Vim-highlighter: Previous", })
+vim.keymap.set("n", "<M-N>", function() vim.cmd("Hi<") end, { desc = "Vim-highlighter: Previous", })
 
 -- :Hi clear or f<C-l> to clear highlights
