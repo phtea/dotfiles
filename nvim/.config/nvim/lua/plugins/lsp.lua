a137dc9a3305312bffa26e15237faf9be6e4a05f
phtea <phtea@mail.com>
2025-12-22 22:30:25 +0300

    stuff

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,9 @@
 vim.pack.add({
 	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/mason-org/mason.nvim",
 })
 
-vim.lsp.enable("pyright")
-vim.lsp.enable("solargraph")
-vim.lsp.enable("clangd")
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("gopls")
-vim.lsp.enable("rust_analyzer")
+vim.lsp.enable(
+	{ "pyright", "solargraph", "clangd", "lua_ls", "gopls", "rust_analyzer" }
+)
 
 vim.diagnostic.config({})
-
-require('mason').setup()
