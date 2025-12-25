4a2efadf3dbb43d128ce3724c61b8e56aca6161b
phtea <phtea@mail.com>
2025-12-25 18:01:23 +0300

    qf improvement

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -1,9 +1,14 @@
-vim.keymap.set("n", "<leader>s", [[:cdo s/\<<C-R><C-W>\>/<C-R><C-W>/gIc<Left><Left><Left><Left>]], {
-	buffer = true,
-	desc = "Replace word under cursor in quickfix list"
-})
+vim.keymap.set("n", "<leader>s", function()
+  local word = vim.fn.expand("<cword>")
+  local replacement = vim.fn.input("Replace '" .. word .. "' with: ")
+  if replacement == "" then return end
+  vim.cmd(string.format([[silent! cdo %%s/\<%s\>/%s/gIc]], word, replacement))
+end, { buffer = true, desc = "Replace word under cursor in quickfix list" })
 
-vim.keymap.set("v", "<leader>s", [[y<Esc>:cdo s/<C-R>"/<C-R>"/gIc<Left><Left><Left><Left>]], {
-	buffer = true,
-	desc = "Replace visual selection in quickfix list"
-})
+vim.keymap.set("v", "<leader>s", function()
+  vim.cmd('normal! "vy')
+  local selection = vim.fn.getreg("v"):gsub("\n", "")
+  local replacement = vim.fn.input("Replace '" .. selection .. "' with: ")
+  if replacement == "" then return end
+  vim.cmd(string.format([[silent! cdo %%s/%s/%s/gIc]], selection, replacement))
+end, { buffer = true, desc = "Replace visual selection in quickfix list" })
