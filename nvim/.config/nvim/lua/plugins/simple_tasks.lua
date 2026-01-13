4cff07ee144c5a4e8b2d333bf65742eb0c93c70a
phtea <phtea@mail.com>
2026-01-13 23:47:28 +0300

    some stuff

--- nvim/.config/nvim/lua/plugins/simple_tasks.lua
+++ nvim/.config/nvim/lua/plugins/simple_tasks.lua
@@ -1,8 +1,10 @@
 vim.pack.add {
-	{ src = "https://github.com/phtea/simple_tasks.nvim" },
+	{ src = "https://github.com/phtea/simple_tasks.nvim", version='dev' }, -- remote plugin
+	-- { src = vim.fn.expand("~/code/lua/simple_tasks.nvim") }, -- local testing
 }
 
 local st = require("simple_tasks")
 st.setup({ picker = "ui" })
 
-vim.keymap.set("n", "<leader>tt", st.pick, { desc = "Project tasks", })
+-- Execute tasks
+vim.keymap.set("n", "<leader>x", st.pick, { desc = "Project tasks", })
