34301f3402767720ecae78c29697a3240ccf7d91
phtea <phtea@mail.com>
2026-01-15 10:59:28 +0300

    remove deprecated multicursor -> i dont need it

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -18,9 +18,8 @@ require("plugins.simple_tasks")
 require("plugins.treesitter")     -- Syntax highlighting
 require("plugins.visimatch")      -- Highlight selection
 require("plugins.vim-highligher") -- Permanently highlight selections
--- require("plugins.multicursor") -- Multicursor plugin (I hope nvim soon has native one so I can get rid of current one but for now I'll stick to it cuz it's more intuitive than :s or :g or cgn the list goes on)
 
--- Navigation
+-- Lsp
 require("plugins.lsp")
 
 -- File management
