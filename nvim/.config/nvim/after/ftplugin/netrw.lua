787b157ef74d20a6d959d09961e31c2cafbec361
phtea <phtea@mail.com>
2026-01-16 23:58:37 +0300

    visual delete in netrw

--- nvim/.config/nvim/after/ftplugin/netrw.lua
+++ nvim/.config/nvim/after/ftplugin/netrw.lua
@@ -1,3 +1,9 @@
 vim.keymap.set("v", "f", ":norm mf<CR>", { buffer = true, silent = true, nowait = true })
 vim.keymap.set("v", "u", ":norm mu<CR>", { buffer = true, silent = true, nowait = true })
 vim.keymap.set("n", "m/", ":g//norm mf<CR>", { buffer = true, silent = true })
+
+local delete_visually = function ()
+	vim.cmd[[norm mf]]
+	vim.cmd[[norm D]]
+end
+vim.keymap.set("v", "d", delete_visually, { buffer = true, silent = true, nowait = true })
