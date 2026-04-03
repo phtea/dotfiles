ef69135ccdf4fe153a6e18455afe6bd1176cf4c3
spushmenkov <spushmenkov@phoenixit.ru>
2026-04-03 12:02:53 +0300

    improved treesitter and F4 for tmux

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,13 +5,20 @@ local regex_languages = { "ruby" }
 
 require('nvim-treesitter').install(languages)
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = languages,
-  callback = function(ev)
+	pattern = languages,
+	callback = function(ev)
 		vim.treesitter.start()
 		if vim.list_contains(regex_languages, ev.match) then vim.bo[ev.buf].syntax = 'ON' end
 	end,
 })
 
-vim.keymap.set("n", "<M-o>", "van", { remap = true, desc = "Treesitter: Init selection" })
-vim.keymap.set("x", "<M-o>", "an", { remap = true, desc = "Treesitter: Expand selection" })
-vim.keymap.set("x", "<M-i>", "in", { remap = true, desc = "Treesitter: Shrink selection" })
+local function ts_select(method)
+	return function()
+		if vim.treesitter.get_parser(nil, nil, { error = false }) then
+			require('vim.treesitter._select')[method](vim.v.count1)
+		end
+	end
+end
+
+vim.keymap.set({ 'n', 'x', 'o' }, '<M-o>', ts_select('select_parent'), { desc = 'TS: Select parent' })
+vim.keymap.set({ 'n', 'x', 'o' }, '<M-i>', ts_select('select_child'),  { desc = 'TS: Select child' })
