c0f4ef50f100e15e032d2caeadfe1f39f9ea1a5e
phtea <phtea@mail.com>
2026-04-04 18:43:45 +0300

    back old cursor

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -10,15 +10,17 @@ vim.cmd [[
 	set exrc " for .nvim.lua (project-specific files)
 	" UI
 	set termguicolors
-	set noshowmode " since cursor is default again I don't really need to see the mode - cursor says it all
 	set winborder=single
-	"set guicursor=n-v-c-i-r-sm-t:block,o:hor20 " operator-pending mode should be horizontal bar; else default
 	"set list listchars=tab:»\ ,trail:·,extends:❯,precedes:❮,nbsp:␣
 
 	set grepprg=rg\ --vimgrep\ --smart-case
 	set grepformat=%f:%l:%c:%m
 
 	set switchbuf=useopen " existing buffer should be reused
+
+	" Cursor stuff
+	"set noshowmode " since cursor is default again I don't really need to see the mode - cursor says it all
+	set guicursor=n-v-c-i-r-sm-t:block,o:hor20 " operator-pending mode should be horizontal bar; else default
 ]]
 
 local paths = {
