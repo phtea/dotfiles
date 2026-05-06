026b0948cc66b82cd3f51e5abbed54e5acc865a6
phtea <phtea@mail.com>
2026-05-06 22:51:42 +0300

    remove PackChanged - no need to encourage plugin usage

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -16,22 +16,6 @@ autocmd('BufRead', {
 	callback = function() vim.bo.filetype = 'yaml' end,
 })
 
--- When package is changed (installed, updated, deleted)
-autocmd('PackChanged', {
-	callback = function(ev)
-		local data = ev.data or vim.v.event -- depending on Neovim version
-
-		if not data then return end
-
-		local kind = data.kind -- 'install' | 'update' | 'delete'
-		local spec = data.spec or {}
-		local name = spec.name or ''
-
-		-- Only for nvim-treesitter, and only on install or update
-		if name == 'nvim-treesitter' and (kind == 'install' or kind == 'update') then vim.cmd('TSUpdate') end
-	end,
-})
-
 autocmd('LspAttach', {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
