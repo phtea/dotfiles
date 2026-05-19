2b703f38dad7e559c89af65492084a4bcc0661c4
phtea <phtea@mail.com>
2026-05-19 22:21:46 +0300

    small update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,8 +1,8 @@
-require("plugins.snacks") -- Picker + Lazygit
-require("plugins.gitsigns") -- Git Signs
+require("plugins.snacks")          -- Picker + Lazygit
+require("plugins.gitsigns")        -- Git Signs
 require("plugins.treesitter")      -- Syntax highlighting
 require("plugins.vim-highlighter") -- Permanently highlight selections
-require("plugins.lspconfig") -- Lsp
-require("plugins.oil") -- File management
-require("plugins.colorscheme") -- Colorscheme
-require("plugins.arduino") -- My arduino development plugin
+require("plugins.lspconfig")       -- Lsp
+require("plugins.oil")             -- File management
+require("plugins.colorscheme")     -- Colorscheme
+require("plugins.arduino")         -- My arduino development plugin
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -8,6 +8,7 @@ vim.lsp.enable({
 	"gopls",
 	"rust_analyzer",
 	"arduino_language_server",
+	"zls",
 })
 
 vim.diagnostic.enable(false) -- disabled by default
