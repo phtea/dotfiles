749ffade3b61f339c3b2fc16255e42410e916f9b
phtea <phtea@mail.com>
2026-05-30 13:02:04 +0300

    added noautocomplete for snacks_picker_input buffer

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,9 +1,9 @@
-vim.cmd([[
+vim.cmd[[
   set hlsearch incsearch ignorecase smartcase number relativenumber tabstop=2 shiftwidth=2 scrolloff=5 signcolumn=yes
   set path+=** lazyredraw cursorline cursorlineopt=number showtabline=0 switchbuf=useopen confirm
   set undofile noswapfile exrc secure guicursor= termguicolors winborder=single noshowmode
   set complete=o,.,w,b completeopt=menuone,noinsert,fuzzy,popup
-]])
+]]
 
 local paths = {
 	undo = vim.fn.stdpath("data") .. "/undo",
