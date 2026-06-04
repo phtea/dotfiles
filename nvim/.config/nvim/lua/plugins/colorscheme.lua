7f349b132b3d3a1ad37ccd297ec7ede508884452
phtea <phtea@mail.com>
2026-06-04 23:50:32 +0300

    improve org-mode style

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,3 +4,7 @@ vim.cmd[[colorscheme vscode]]
 vim.api.nvim_set_hl(0, "Pmenu", { link = "ModeMsg" })
 vim.api.nvim_set_hl(0, "netrwMarkFile", { link = "MatchParen" })
 vim.api.nvim_set_hl(0, "ModeMsg", { link = "Normal" })
+
+-- org-mode highlights
+vim.api.nvim_set_hl(0, "@org.keyword.done", { link = "Comment" })
+vim.api.nvim_set_hl(0, "@org.agenda.scheduled", { link = "Comment" })
--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,7 +1,13 @@
-vim.pack.add({ "https://github.com/nvim-orgmode/orgmode" })
+vim.pack.add({
+	"https://github.com/nvim-orgmode/orgmode",
+	"https://github.com/nvim-orgmode/org-bullets.nvim"
+})
 
 require("orgmode").setup({
-  org_agenda_files = "~/org/**/*",
-  org_default_notes_file = "~/org/refile.org",
-  org_todo_keywords = { "TODO(t)", "NEXT(n)", "WAIT(w)", "|", "DONE(d)", "CANCELLED(c)", },
+	org_agenda_files = "~/{org,notes}/**/*",
+	org_default_notes_file = "~/org/refile.org",
+	org_todo_keywords = { "TODO(t)", "NEXT(n)", "WAIT(w)", "|", "DONE(d)", "CANCELLED(c)", },
+	org_startup_indented = true,   
 })
+
+require('org-bullets').setup()
