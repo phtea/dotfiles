c6bcdb6b4af14894547337d84a9763f012d98118
spushmenkov <spushmenkov@phoenixit.ru>
2026-07-13 15:07:22 +0300

    adjustments

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,3 +2,4 @@ vim.cmd[[colorscheme vscode]]
 vim.api.nvim_set_hl(0, "Pmenu", { link = "ModeMsg" })
 vim.api.nvim_set_hl(0, "netrwMarkFile", { link = "MatchParen" })
 vim.api.nvim_set_hl(0, "ModeMsg", { link = "Normal" })
+vim.api.nvim_set_hl(0, "SnacksPickerMatch", { underline = true, })
